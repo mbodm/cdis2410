21d8e7a87de2deb16f2b9f983c97060e3bb3a9aa
MBODM <webmaster@mbodm.com>
2024-03-20 04:18:03 +0100

    Initial commit

---
+++ src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace ChangeDisplayInputSourceDellU2410
+{
+    internal static class Helper
+    {
+        private const string Tail = "[cdis2410.exe] (by MBODM 2024)";
+
+        public static string ExeName = "cdis2410.exe";
+
+        public static string AppName = "ChangeDisplayInputSource Dell U2410";
+
+        // For Console Apps this seems to be the most simple way, in .NET 5 or later.
+        // It's the counterpart of the "Version" entry, declared in the .csproj file.
+        public static string AppVersion => Assembly.
+            GetEntryAssembly()?.
+            GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.
+            InformationalVersion ?? string.Empty;
+
+        public static string AppTitle => AppVersion == string.Empty ? $"{AppName} {Tail}" : $"{AppName} {AppVersion} {Tail}";
+    }
+}
---
+++ src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Program.cs
@@ -0,0 +1,49 @@
+﻿using ChangeDisplayInputSourceDellU2410;
+
+Console.WriteLine();
+Console.WriteLine(Helper.AppTitle);
+Console.WriteLine();
+
+var hWindow = WinApi.GetDesktopWindow();
+var hMonitor = WinApi.MonitorFromWindow(hWindow, WinApi.MONITOR_DEFAULTTOPRIMARY);
+
+if (WinApi.GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor, out uint numberOfPhysicalMonitors))
+{
+    if (numberOfPhysicalMonitors > 0)
+    {
+        var physicalMonitors = new WinApi.PHYSICAL_MONITOR[numberOfPhysicalMonitors];
+
+        if (WinApi.GetPhysicalMonitorsFromHMONITOR(hMonitor, numberOfPhysicalMonitors, physicalMonitors))
+        {
+            var hPhysicalMonitor = physicalMonitors[0].hPhysicalMonitor;
+            var szPhysicalMonitorDescription = physicalMonitors[0].szPhysicalMonitorDescription;
+
+            Console.WriteLine($"Display description: {szPhysicalMonitorDescription}");
+
+            if (WinApi.GetVCPFeatureAndVCPFeatureReply(hPhysicalMonitor, 0x60, out _, out uint currentValue, out _))
+            {
+                Console.WriteLine($"Current VCP60 value: {currentValue} ({DellU2410.GetInputSourceNameFromVCP60Value(currentValue)})");
+
+                if (args.Length > 0)
+                {
+                    var changedValue = DellU2410.GetVCP60ValueFromInputSourceName(args[0]);
+
+                    if (changedValue == 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Error: Given input source name is not supported.");
+
+                        return;
+                    }
+
+                    Console.WriteLine($"Changed VCP60 value: {changedValue} ({DellU2410.GetInputSourceNameFromVCP60Value(changedValue)})");
+
+                    WinApi.SetVCPFeature(hPhysicalMonitor, 0x60, changedValue);
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Have a nice day.");
