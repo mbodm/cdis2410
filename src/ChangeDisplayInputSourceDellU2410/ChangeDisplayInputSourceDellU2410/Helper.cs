44e20dd833fca73fbcfdee24bbe83156e593193d
Marcel Beck <MBeck@ad.jtl-software.de>
2024-03-23 18:12:56 +0100

    Finalized the application. Ready to release 1.0.0 version.

--- src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Helper.cs
+++ src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Helper.cs
@@ -4,19 +4,34 @@ namespace ChangeDisplayInputSourceDellU2410
 {
     internal static class Helper
     {
-        private const string Tail = "[cdis2410.exe] (by MBODM 2024)";
+        private const string Tail = "(by MBODM 2024)";
 
-        public static string ExeName = "cdis2410.exe";
+        public static string AppName => "ChangeDisplayInputSource Dell U2410";
+        public static string AppVersion => GetVersionFromProject();
+        public static string AppTitle => $"{AppName} {AppVersion} {Tail}";
+        public static string ExeName => $"{GetAssemblyNameFromProject().ToLower()}.exe";
+        public static string ExeDescription => $"The {ExeName} is a tiny CLI tool, using DDC to get/set the input source of my Dell U2410 display.";
 
-        public static string AppName = "ChangeDisplayInputSource Dell U2410";
+        private static string GetVersionFromProject()
+        {
+            // For Console Apps this seems to be the most simple way, in .NET 5 or later.
+            // It's the counterpart of the "Version" entry, declared in the .csproj file.
 
-        // For Console Apps this seems to be the most simple way, in .NET 5 or later.
-        // It's the counterpart of the "Version" entry, declared in the .csproj file.
-        public static string AppVersion => Assembly.
-            GetEntryAssembly()?.
-            GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.
-            InformationalVersion ?? string.Empty;
+            return Assembly.
+                GetEntryAssembly()?.
+                GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.
+                InformationalVersion ?? "0.0.0";
+        }
 
-        public static string AppTitle => AppVersion == string.Empty ? $"{AppName} {Tail}" : $"{AppName} {AppVersion} {Tail}";
+        private static string GetAssemblyNameFromProject()
+        {
+            // Not the project's name shall be used as exe/assembly file name, when published.
+            // It's the counterpart of the "AssemblyName" entry, declared in the .csproj file.
+
+            return Assembly.
+                GetExecutingAssembly()?.
+                GetName()?.
+                Name ?? "UNKNOWN";
+        }
     }
 }
--- src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Program.cs
+++ src/ChangeDisplayInputSourceDellU2410/ChangeDisplayInputSourceDellU2410/Program.cs
@@ -3,47 +3,52 @@
 Console.WriteLine();
 Console.WriteLine(Helper.AppTitle);
 Console.WriteLine();
+Console.WriteLine(Helper.ExeDescription);
 
 var hWindow = WinApi.GetDesktopWindow();
 var hMonitor = WinApi.MonitorFromWindow(hWindow, WinApi.MONITOR_DEFAULTTOPRIMARY);
 
-if (WinApi.GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor, out uint numberOfPhysicalMonitors))
+if (!WinApi.GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor, out uint numberOfPhysicalMonitors))
+    ShowErrorAndExit("Could not determine number of physical monitors.", 1);
+
+if (numberOfPhysicalMonitors <= 0)
+    ShowErrorAndExit("Could not found any physical monitors.", 2);
+
+var physicalMonitors = new WinApi.PHYSICAL_MONITOR[numberOfPhysicalMonitors];
+
+if (!WinApi.GetPhysicalMonitorsFromHMONITOR(hMonitor, numberOfPhysicalMonitors, physicalMonitors))
+    ShowErrorAndExit("Could not determine primary physical monitor.", 3);
+
+var hPhysicalMonitor = physicalMonitors[0].hPhysicalMonitor;
+var szPhysicalMonitorDescription = physicalMonitors[0].szPhysicalMonitorDescription;
+
+Console.WriteLine();
+Console.WriteLine($"Display description: {szPhysicalMonitorDescription}");
+
+if (!WinApi.GetVCPFeatureAndVCPFeatureReply(hPhysicalMonitor, 0x60, out _, out uint currentValue, out _))
+    ShowErrorAndExit("Could not get current VCP60 value.", 4);
+
+Console.WriteLine($"Current VCP60 value: {currentValue} ({DellU2410.GetInputSourceNameFromVCP60Value(currentValue)})");
+
+if (args.Length > 0)
 {
-    if (numberOfPhysicalMonitors > 0)
-    {
-        var physicalMonitors = new WinApi.PHYSICAL_MONITOR[numberOfPhysicalMonitors];
+    var changedValue = DellU2410.GetVCP60ValueFromInputSourceName(args[0]);
+    if (changedValue == 0)
+        ShowErrorAndExit("Given input source name is not supported.", 5);
 
-        if (WinApi.GetPhysicalMonitorsFromHMONITOR(hMonitor, numberOfPhysicalMonitors, physicalMonitors))
-        {
-            var hPhysicalMonitor = physicalMonitors[0].hPhysicalMonitor;
-            var szPhysicalMonitorDescription = physicalMonitors[0].szPhysicalMonitorDescription;
+    if (!WinApi.SetVCPFeature(hPhysicalMonitor, 0x60, changedValue))
+        ShowErrorAndExit("Could not set VCP60 value.", 5);
 
-            Console.WriteLine($"Display description: {szPhysicalMonitorDescription}");
-
-            if (WinApi.GetVCPFeatureAndVCPFeatureReply(hPhysicalMonitor, 0x60, out _, out uint currentValue, out _))
-            {
-                Console.WriteLine($"Current VCP60 value: {currentValue} ({DellU2410.GetInputSourceNameFromVCP60Value(currentValue)})");
-
-                if (args.Length > 0)
-                {
-                    var changedValue = DellU2410.GetVCP60ValueFromInputSourceName(args[0]);
-
-                    if (changedValue == 0)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Error: Given input source name is not supported.");
-
-                        return;
-                    }
-
-                    Console.WriteLine($"Changed VCP60 value: {changedValue} ({DellU2410.GetInputSourceNameFromVCP60Value(changedValue)})");
-
-                    WinApi.SetVCPFeature(hPhysicalMonitor, 0x60, changedValue);
-                }
-            }
-        }
-    }
+    Console.WriteLine($"Changed VCP60 value: {changedValue} ({DellU2410.GetInputSourceNameFromVCP60Value(changedValue)})");
 }
 
 Console.WriteLine();
 Console.WriteLine("Have a nice day.");
+Environment.Exit(0);
+
+static void ShowErrorAndExit(string errorMessage, int exitCode)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Error: {errorMessage}");
+    Environment.Exit(exitCode);
+};
